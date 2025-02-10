7dce696a295bbf3ecd216350aa8c984c69087588
wqba <wabarurse@gmail.com>
2025-02-10 00:15:30 -0500

    me.jpeg

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,7 +15,7 @@ function About() {
       </div>
       <div className="scroll-fade bg-color flex justify-center mx-auto px-4 py-24">
         <div className="profile-picture">
-          <img src="/images/me.jpeg" alt="profile" className="w-full h-full rounded-full" />
+          <img src={`${process.env.PUBLIC_URL}/images/me.jpeg`} alt="profile" className="w-full h-full rounded-full" />
         </div>
         <div className="flex flex-col space-y-4 max-w-screen-lg">
           <h1 className="text-6xl font-nunito font-bold text-color-1 px-16">Hi 👋, I'm Richard</h1>
